4f0a2f71d16ef19185b68bbaf67e84f184aba000
GelenMR <gelen.montilla.r@gmail.com>
2019-04-04 13:01:23 -0300

    Revision de  console.log en js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-//FUNCION PARA GUARDAR LA VARIABLE DEL MENSAJE A CIFRAR CON EL EVENTO CLICK
+//DECLARACION DE VARIABLES
 let textToEncode;
 let offset;
 let resolved;
 let resolved2;
+//GUARDAR MENSAJE A CIFRAR CON EL EVENTO CLICK 
+//BOTON CIFRAR
 const btnSendTextToEncode = document.getElementById("sendTextToEncode");
 btnSendTextToEncode.addEventListener("click",() => {
-    
     //GUARDAR EL VALOR DE LA VARIABLE DEL MENSAJE A CIFRAR
     textToEncode = document.getElementById("textToEncode").value;
     console.log(textToEncode);  
@@ -15,6 +16,7 @@ btnSendTextToEncode.addEventListener("click",() => {
     resolved = window.cipher.encode(offset, textToEncode);
     document.getElementById("resultEncode").innerHTML = resolved;
 });
+//BOTON DESCIFRAR
 const btnSendTextToDecode = document.getElementById("sendTextToDecode");
 btnSendTextToDecode.addEventListener("click",() => {
     
@@ -24,7 +26,7 @@ btnSendTextToDecode.addEventListener("click",() => {
     //GUARDAR EL VALOR DEL DESPLAZAMIENTO (OFFSET)
     offset = parseInt(document.getElementById("offset").value);
     console.log(offset);
-    resolved = window.cipher.decode(offset, textToEncode);
-    document.getElementById("resultEncode").innerHTML = resolved;
+    resolved2 = window.cipher.decode(offset, textToEncode);
+    document.getElementById("resultEncode").innerHTML = resolved2;
 });
 
